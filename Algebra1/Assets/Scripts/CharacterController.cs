4829f5c0489b0ce6cb67d9ecc1a21c9956765b5a
Saul Augusto Gasca Farrera <sgfarrera115@hotmail.com>
2021-02-18 16:48:51 -0600

    Chatracter Moveemnt Horizontal Added, Rigidbody Velocito added on Character

---
+++ Algebra1/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    [Header("Input")]
+    public float walkSpeed;
+    public float horizontalMovement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(walkSpeed * horizontalMovement, rb.velocity.y);
+    }
+}
