82b0845e7d63727ff2b74e32d519cfd07556f48d
Saul Augusto Gasca Farrera <sgfarrera115@hotmail.com>
2021-02-22 19:25:06 -0600

    Platform Aotonomous with Move Right Left and Move Up Down added and Enemy 3 added with a Little bug

---
+++ Algebra1/Assets/Scripts/AutonomousPlatform.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutonomousPlatform : MonoBehaviour
+{
+
+    [Header("Platform")]
+    public float platformSpeed;
+    public Transform rightLimit;
+    public Transform leftLimit;
+    public Transform upLimit;
+    public Transform downLimit;
+    public Transform checkPosition;
+    public float tolarence = 1.0f;
+    public bool OtherBehaviour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!OtherBehaviour)
+        {
+            MoveRightToLeft();
+        }
+        else
+        {
+            MoveUpDown();
+        }
+
+    }
+
+    void MoveRightToLeft()
+    {
+      if((transform.position.x < rightLimit.position.x + tolarence && transform.position.x > rightLimit.position.x - tolarence) || (transform.position.x < leftLimit.position.x + tolarence && transform.position.x > leftLimit.position.x - tolarence))
+      {
+            platformSpeed = -platformSpeed;
+            Debug.Log("Arrived");
+
+      }
+
+        transform.Translate(platformSpeed * Time.deltaTime,0,0);
+    }
+
+    void MoveUpDown()
+    {
+        if((transform.position.y < upLimit.position.y + tolarence && transform.position.y > upLimit.position.y - tolarence) || (transform.position.y < downLimit.position.y + tolarence && transform.position.y > downLimit.position.y -tolarence))
+        {
+            platformSpeed = -platformSpeed;
+        }
+
+
+        transform.Translate(0, platformSpeed * Time.deltaTime, 0);
+    }
+}
---
+++ Algebra1/Assets/Scripts/Enemy3.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy3 : MonoBehaviour
+{
+    [Header("Enemy 3")]
+    public Rigidbody2D rb;
+    public bool touchingEdgeLeft;
+    public bool touchingEdgeRight;
+    public float enemySpeed;
+    public Transform rightCheck;
+    public Transform leftCheck;
+    public float checkRadius;
+    public LayerMask platform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AutonomousPlatform();
+    }
+
+    void AutonomousPlatform()
+    {
+        touchingEdgeLeft = Physics2D.OverlapCircle(leftCheck.position, checkRadius, platform);
+        touchingEdgeRight = Physics2D.OverlapCircle(rightCheck.position, checkRadius, platform);
+
+        if(touchingEdgeLeft || touchingEdgeRight)
+        {
+            enemySpeed = -enemySpeed;
+        }
+
+        rb.velocity = new Vector2(enemySpeed,rb.velocity.y);
+    }
+}
